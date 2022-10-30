0909c518b2150df97fbc8b128cc5ea4ad0b06bf9
Finn Pauls <mbp@pauls.dev>
2022-10-30 18:00:04 +0100

    Bats start sucking and die if they exceed capacity

--- Assets/Bat.cs
+++ Assets/Bat.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Bat : MonoBehaviour
 {
@@ -8,15 +9,47 @@ public class Bat : MonoBehaviour
 
     private Villager following;
 
+    public TextMeshPro bloodText;
+
+    private float blood = 0;
+
+    bool isSucking = false;
+
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
-        following = FindRandomVillager();
+        following = FindClosestVillager();
     }
 
     void Update()
     {
-        FollowVillager();
+        if (isSucking)
+        {
+            SuckVillager();
+        }
+        else
+        {
+            FollowVillager();
+        }
+    }
+
+    void SuckVillager()
+    {
+        blood += Time.deltaTime * 10;
+        var rounded = Mathf.RoundToInt(blood);
+        bloodText.text = rounded + "/100";
+        if (blood >= 100)
+        {
+            Destroy(gameObject);
+            Destroy(following.gameObject);
+        }
+    }
+
+    void StartSucking()
+    {
+        isSucking = true;
+        following.StopWalking();
+        rigidBody.velocity = Vector2.zero;
     }
 
     void FollowVillager()
@@ -27,12 +60,28 @@ public class Bat : MonoBehaviour
         var distance = direction.magnitude;
         if (distance < 0.1f)
         {
-            following.StopWalking();
-            rigidBody.velocity = Vector2.zero;
+            StartSucking();
             return;
         }
 
         rigidBody.velocity = direction.normalized * 5;
+    }
+
+    Villager FindClosestVillager()
+    {
+        var villagers = FindObjectsOfType<Villager>();
+        Villager closest = null;
+        var closestDistance = float.MaxValue;
+        foreach (var villager in villagers)
+        {
+            var distance = (villager.transform.position - transform.position).magnitude;
+            if (distance < closestDistance)
+            {
+                closest = villager;
+                closestDistance = distance;
+            }
+        }
+        return closest;
     }
 
     Villager FindRandomVillager()
